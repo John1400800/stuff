2a4c96c0d0953603b2cc6a829668521be567190f
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-04-14 00:42:36 +0300

    Update config.h

--- configs/slstatus/config.h
+++ configs/slstatus/config.h
@@ -1,11 +1,10 @@
 static const struct arg args[] = {
 	/* function        format          argument */
-	{ cpu_perc,      "cpu: %s%% | ",  NULL	        },
-	{ ram_used, 	   "ram: %s/", 	    NULL	        },
-	{ ram_total, 	   "%s | ",  	      NULL 	        },
-	{ datetime,  	   "%s | ",         "%a %d %b %T" },
-	{ keymap,  	     "%s | ",  	      NULL  	      },
-	{ wifi_perc,     "wifi: %s | ",   "wlp0s20f3"   },
-	{ battery_state, "%s",            "BAT1" 	      },
-	{ battery_perc,  "%s%%",          "BAT1" 	      },
+	{ cpu_perc,	 "CPU: %s%% | ",	NULL		},
+	{ ram_perc,	 "RAM: %s%% | ",	NULL		},
+	{ wifi_perc,     "NET: %s%% | ",	"wlp0s20f3"	},
+	{ battery_state, "BAT: %s",		"BAT1"		},
+	{ battery_perc,  "%s%% | ",		"BAT1"		},
+	{ keymap,	 "%s | ",		NULL		},
+	{ datetime,	 "%s",			"%a %d %b %R"	},
 };
