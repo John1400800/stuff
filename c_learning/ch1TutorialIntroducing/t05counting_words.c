96e1300cd10758b60f090fc4e8109295710dac41
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-04-29 16:31:17 +0300

    Update t05counting_words.c

--- c_learning/ch1TutorialIntroducing/t05counting_words.c
+++ c_learning/ch1TutorialIntroducing/t05counting_words.c
@@ -1,48 +1,18 @@
 #include <stdio.h>
 
-#define MAXLINE 100
+#define isspace(c) ((c)==' '||(unsigned)(c)-'\t'<5)
 
-// worked only in ascii encoding
-char is_space(char c) { return (c >= 9 && c <= 13) || c == 32; }
-
-unsigned get_str(char *s, unsigned lim) {
-    unsigned i = 0;
-    while (i < lim &&
-           !((s[i] = (char)getchar()) == '\n' && i > 0 && s[i - 1] == '\n')) {
-        ++i;
-    }
-
-    if (s[i - 1] == '\n' && s[i] == '\n') {
-        --i;
-    }
-    s[i] = '\0';
-    return i;
-}
-
-unsigned world_count(char *s) {
-    unsigned countr = 0;
-    char islastsp = 1;
-    unsigned i = 0;
-    while (s[i] != '\0') {
-        if (is_space(s[i])) {
-            islastsp = 1;
-        } else if (islastsp == 1) {
-            islastsp = 0;
-            ++countr;
-        }
-        ++i;
-    }
-    return countr;
-}
+enum { OUT, IN };
 
 int main(void) {
-    char s[MAXLINE + 1];
-
-    printf("input str: ");
-    while (!get_str(s, MAXLINE)) {
-        printf("try again: ");
-    }
-    
-
-    printf("input str has %d words", world_count(s));
+    int state, c, nw;
+    for (nw=0, state=OUT; (c=getchar())!=EOF; )
+        if (isspace(c))
+            state=OUT;
+        else if (state==OUT) {
+            ++nw;
+            state=IN;
+        }
+    printf("%d\n", nw);
+    return 0;
 }
