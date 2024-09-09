8d0f50b3e5c8cb725bbaf44b1311dd06fd8f8378
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-09-09 21:11:44 +0300

    Create task13.c

---
+++ c_learning/home_works/task13.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#ifndef _CTYPE_H
+#define isspace(c) ((c) == ' ' || (unsigned)(c)-'\t' < 5)
+#endif // _CTYPE_H
+
+char *rev_words_in_sentence(const char *src);
+
+int main(void) {
+    printf("%s\n", rev_words_in_sentence("hello world"));
+    return EXIT_SUCCESS;
+}
+
+char *rev_words_in_sentence(const char *src) {
+    char *res = malloc(strlen(src)+1);
+    strcpy(res, src);
+    char *start_word_ptr, *end_word_ptr;
+    for (start_word_ptr = end_word_ptr = res;
+            *start_word_ptr != '\0';
+            ++end_word_ptr)
+    {
+        if (isspace(*start_word_ptr)) {
+            ++start_word_ptr;
+            continue;
+        }
+        if (isspace(end_word_ptr[1]) || end_word_ptr[1] == '\0')
+        {
+            char *temp_ptr;
+            for (temp_ptr = end_word_ptr+1;
+                    start_word_ptr < end_word_ptr;
+                    --end_word_ptr, ++start_word_ptr)
+            {
+                char temp = *start_word_ptr;
+                *start_word_ptr = *end_word_ptr;
+                *end_word_ptr   = temp;
+            }
+            start_word_ptr = end_word_ptr = temp_ptr;
+        }
+    }
+    return res;
+}
+
