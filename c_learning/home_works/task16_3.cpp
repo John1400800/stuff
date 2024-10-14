99c55084d0a19b3ab692577777c9c0feacb1d539
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-10-14 03:13:21 +0300

    Update task16_3.cpp

--- c_learning/home_works/task16_3.cpp
+++ c_learning/home_works/task16_3.cpp
@@ -7,93 +7,76 @@
 #include <utility>
 #include <iostream>
 
-// #define DEBUG
-
-#define cfcnt   first
-#define degree second
-
 using Term = std::pair<int32_t, int32_t>;
 
-Term parseStrToTerm(std::string term) {
-    Term res( 0, 0 );
-    if (term.empty())
-        return res;
-    size_t xPos{ term.find('x') };
-    if (xPos == std::string::npos) {
-        res.cfcnt = std::stoi(term);
-        return res;
-    }
-    std::string cfcntStr{ term.substr(0, xPos) };
-    if (cfcntStr.empty() || cfcntStr == "+")
-        res.cfcnt = +1;
-    else if (cfcntStr == "-")
-        res.cfcnt = -1;
-    else
-        res.cfcnt = std::stoi(cfcntStr);
-    size_t powPos{ term.find('^') };
-    if (powPos == std::string::npos)
-        res.degree = 1;
-    else
-        res.degree = std::stoi(term.substr(powPos+1));
-    return res;
-}
-
-void parseStrToPolynomial(const std::string& str, std::list<Term>& res) {
-    size_t start{ 0 };
-    size_t pos  { start };
-    while (pos != std::string::npos) {
-        pos = str.find_first_of("+-", start + 1);
-        Term term = parseStrToTerm(str.substr(start, pos - start));
-        auto it{ res.begin() }; // separate this VVV
-        bool insrtd{ false };
-        while (!insrtd && it != res.end()) {
-            if (term.degree == it->degree) {
-                it->cfcnt += term.cfcnt;
-                if (it->cfcnt == 0)
-                    it = res.erase(it);
-                insrtd = true;
-            } else if (it->degree < term.degree) {
-                res.insert(it, term);
-                insrtd = true;
+constexpr Term parseStrToTerm(std::string_view term) {
+    int32_t cfcnt = 1, degree = 1;
+    auto xPos = term.find('x');
+    if (xPos != std::string_view::npos) {
+        if (xPos > 0) {
+            if (term[xPos - 1] == '+' || term[xPos - 1] == '-') {
+                cfcnt = term[xPos - 1] == '+' ? 1 : -1;
+            } else {
+                cfcnt = std::stoi(std::string(term.substr(0, xPos)));
             }
-            ++it;
         }
-        if (!insrtd)
-            res.push_back(term);
-        start = pos;
+        auto powPos = term.find('^', xPos);
+        if (powPos == std::string_view::npos) {
+            degree = 1;
+        } else {
+            degree = std::stoi(std::string(term.substr(powPos + 1)));
+        }
+    } else {
+        cfcnt = std::stoi(std::string(term));
+        degree = 0;
+    }
+    return { cfcnt, degree };
+}
+
+void insertTerm(std::list<Term>& poly, const Term& term) {
+    for (auto it = poly.begin(); it != poly.end(); ++it) {
+        if (it->second == term.second) {
+            it->first += term.first;
+            if (it->first == 0)
+                poly.erase(it);
+            return;
+        } else if (it->second < term.second) {
+            poly.insert(it, term);
+            return;
+        }
+    }
+    poly.push_back(term);
+}
+
+void parseStrToPolynomial(std::string_view str, std::list<Term>& poly) {
+    size_t start = 0, len = str.size();
+    while (start < len) {
+        size_t next = str.find_first_of("+-", start + 1);
+        if (next == std::string_view::npos) next = len;
+        insertTerm(poly, parseStrToTerm(str.substr(start, next - start)));
+        start = next;
     }
 }
 
-std::ostream& operator<<(std::ostream& out, Term term) {
-#ifdef DEBUG
-    return out << "{ " << term.cfcnt << ", " << term.degree << " }";
-#else
-    if (term.cfcnt < 0) {
-        out << '-';
-        term.cfcnt = -term.cfcnt;
-    } else
-        out << '+';
-    if (term.cfcnt != 0) {
-        if (term.degree == 0)
-            out << term.cfcnt;
-        else {
-            if (term.cfcnt != 1)
-                out << term.cfcnt;
-            out << 'x';
-            if (term.degree != 1)
-                out << '^' << term.degree;
-        }
-    } else
-        out << 0;
+std::ostream& operator<<(std::ostream& out, const Term& term) {
+    if (term.first == 0)
+        return out << 0;
+    if (term.first > 0) out << "+";
+    else out << "-";
+    if (std::abs(term.first) != 1 || term.second == 0)
+        out << std::abs(term.first);
+    if (term.second > 0) {
+        out << 'x';
+        if (term.second != 1) out << '^' << term.second;
+    }
     return out;
-#endif
 }
 
 int main() {
-    std::list<Term> polynomial{};
+    std::list<Term> polynomial;
     parseStrToPolynomial("2x^3-1+3x^4-0+x^4", polynomial);
-    for (const Term& term : polynomial)
-        std::cout << term << '\n';
+    for (const auto& term : polynomial)
+        std::cout << term << " ";
     std::cout << '\n';
     return EXIT_SUCCESS;
 }
