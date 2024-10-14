1a2199ac70ca97265b66e40f5074f8d01b094305
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-10-14 17:31:48 +0300

    Update task16_3.cpp

--- c_learning/home_works/task16_3.cpp
+++ c_learning/home_works/task16_3.cpp
@@ -1,40 +1,45 @@
-#include <cstdlib>
-#include <cstdint>
-#include <cmath>
-#include <list>
-#include <string>
-#include <string_view>
-#include <utility>
-#include <iostream>
+#include <cstdlib>      // for EXIT_SUCCESS, abs
+#include <cstdint>      // for int32_t
+#include <list>         // std::list
+#include <string>       // std::string
+#include <string_view>  // std::string_view
+#include <utility>      // for std::pair
+#include <iostream>     // std::cout, std::cin
+
+// #define DEBUG_PRINT
 
 using Term = std::pair<int32_t, int32_t>;
 
-constexpr Term parseStrToTerm(std::string_view term) {
-    int32_t cfcnt = 1, degree = 1;
-    auto xPos = term.find('x');
-    if (xPos != std::string_view::npos) {
-        if (xPos > 0) {
-            if (term[xPos - 1] == '+' || term[xPos - 1] == '-') {
-                cfcnt = term[xPos - 1] == '+' ? 1 : -1;
-            } else {
-                cfcnt = std::stoi(std::string(term.substr(0, xPos)));
-            }
-        }
-        auto powPos = term.find('^', xPos);
-        if (powPos == std::string_view::npos) {
-            degree = 1;
-        } else {
-            degree = std::stoi(std::string(term.substr(powPos + 1)));
-        }
-    } else {
+Term parseStrToTerm(std::string_view term) {
+    int32_t cfcnt { 1 },
+            degree{ 1 };
+    size_t xPos{ term.find('x') };
+    if (xPos == std::string_view::npos) { // if x not exist
         cfcnt = std::stoi(std::string(term));
         degree = 0;
+    } else { 
+        if (xPos > 0)
+            switch (term[xPos-1]) {
+            case '-':
+                cfcnt = -1;
+                break;
+            case '+':
+                cfcnt = +1;
+                break;
+            default:
+                cfcnt = std::stoi(std::string(term.substr(0, xPos)));
+            }
+        size_t powPos{ term.find('^', xPos) };
+        if (powPos == std::string_view::npos)
+            degree = 1;
+        else
+            degree = std::stoi(std::string(term.substr(powPos + 1)));
     }
     return { cfcnt, degree };
 }
 
 void insertTerm(std::list<Term>& poly, const Term& term) {
-    for (auto it = poly.begin(); it != poly.end(); ++it) {
+    for (auto it{ poly.begin() }; it != poly.end(); ++it) {
         if (it->second == term.second) {
             it->first += term.first;
             if (it->first == 0)
@@ -45,30 +50,36 @@ void insertTerm(std::list<Term>& poly, const Term& term) {
             return;
         }
     }
-    poly.push_back(term);
+    poly.push_back(term); // if term has the lowest degree put to the end
 }
 
 void parseStrToPolynomial(std::string_view str, std::list<Term>& poly) {
-    size_t start = 0, len = str.size();
+    size_t start {0},
+           len   { str.size() };
     while (start < len) {
-        size_t next = str.find_first_of("+-", start + 1);
-        if (next == std::string_view::npos) next = len;
+        size_t next{ str.find_first_of("+-", start + 1) };
+        if (next == std::string_view::npos)
+            next = len;
         insertTerm(poly, parseStrToTerm(str.substr(start, next - start)));
         start = next;
     }
 }
 
 std::ostream& operator<<(std::ostream& out, const Term& term) {
+#ifndef DEBUG_PRINT
     if (term.first == 0)
         return out << 0;
-    if (term.first > 0) out << "+";
-    else out << "-";
+    out << (term.first < 0? '-' : '+');
     if (std::abs(term.first) != 1 || term.second == 0)
         out << std::abs(term.first);
     if (term.second > 0) {
         out << 'x';
-        if (term.second != 1) out << '^' << term.second;
+        if (term.second != 1)
+            out << '^' << term.second;
     }
+#else
+    out << "{ " << term.first << ", " << term.second << " }";
+#endif
     return out;
 }
 
@@ -82,8 +93,6 @@ int main() {
             break;
         parseStrToPolynomial(input, polynomial);
     }
-        
-
     std::cout << "Полином:\n";
     for (const auto& term : polynomial)
         std::cout << term << " ";
