2bece1573c66733d5420440ad06c8f1b823e22f6
John1400800 <94189388+John1400800@users.noreply.github.com>
2024-11-17 21:42:53 +0300

    Create task19_2.cpp

---
+++ c_learning/home_works/task19_2.cpp
@@ -0,0 +1,53 @@
+#include <cstdlib>
+#include <cstdint>
+#include <set>
+#include <sstream>
+#include <iostream>
+#include <algorithm> // Для std::set_difference
+
+std::istream& operator>>(std::istream& in, std::set<uint32_t>& set) {
+    std::string inputStr;
+    while (true) {
+        std::getline(in, inputStr);
+        std::stringstream sstream(inputStr);
+        if (uint32_t num; sstream >> num)
+            set.insert(num);
+        else if (!inputStr.empty())
+            std::cout << "Wrong input, try again\n";
+        else
+            break;
+    }
+    return in;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::set<T>& set) {
+    for (const T& el : set)
+        out << el << ' ';
+    return out;
+}
+
+template <typename T>
+std::set<T> operator-(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> res;
+    std::set_difference(a.begin(), a.end(), b.begin(), b.end(), std::inserter(res, res.end()));
+    return res;
+}
+
+template <typename T>
+std::set<T> operator&(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> res;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(res, res.end()));
+    return res;
+}
+
+int main() {
+    std::set<uint32_t> setA, setB;
+    std::cout << "Enter set A: ";
+    std::cin >> setA;
+    std::cout << "Enter set B: ";
+    std::cin >> setB;
+
+    std::cout << "(A \\ B) & (A & B)==null: " << std::boolalpha << (((setA - setB) & (setA & setB)) == std::set<uint32_t>{}) << '\n';
+    return EXIT_SUCCESS;
+}
