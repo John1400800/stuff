a369e97ef971bf277048410ea9c79de417621b3f
John1400800 <94189388+John1400800@users.noreply.github.com>
2024-11-26 21:02:19 +0300

    Update main.cpp

--- c_learning/home_works/task20/main.cpp
+++ c_learning/home_works/task20/main.cpp
@@ -1,10 +1,13 @@
+#include <cstdlib>
+#include <cstdint>
 #include <iostream>
+
 #include "max.hpp"
 
 int main(int32_t, const char **) {
-    std::cout << "Поочережно введитке a b: ";
+    std::cout << "Поочережно введитке a и b: ";
     uint32_t a, b;
     std::cin >> a >> b;
-    std::cout << "большее: " << max(a, b) << '\n';
+    std::cout << "болбшее из a и b: " << max(a, b) << '\n';
     return EXIT_SUCCESS;
 }
