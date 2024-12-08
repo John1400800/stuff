8f7974f9b40382ab9f6f8b9aa9811eb9cd5bf61c
John1400800 <94189388+John1400800@users.noreply.github.com>
2024-12-08 20:18:31 +0300

    Update task18_3a.c

--- c_learning/home_works/task18_3a.c
+++ c_learning/home_works/task18_3a.c
@@ -1,70 +1,104 @@
-#include <stdlib.h>
-#include <stdint.h>
-#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define BUFFSIZE 2048
-typedef enum exitStatus {
-    FAILURE = 0,
-    SUCCESS = 1
-} exitStatus;
+// #define FILENAME "input.txt"
+#define MAX_LINE_LENGTH 1024
 
-bool readText(char *text, const char* fileName) {
-    FILE *file = fopen(fileName, "r");
-    if (!file) {
-        perror("\nFile open error\n");
-        return FAILURE;
+typedef struct {
+    char **lines; // dynamic array of 'char*' - lines
+    size_t numLines;
+    size_t maxLines;
+} TextLines;
+
+bool readTextFromFile(TextLines *lines, FILE *file) {
+    if (!lines->lines) {
+        lines->numLines = 0;
+        lines->maxLines = 10;
+        lines->lines = malloc(lines->maxLines * sizeof(char *));
     }
-    char ch;
-    size_t i=0;
-    while (i < BUFFSIZE-1 && (ch = fgetc(file)) != '*' && ch != EOF)
-        text[i++] = ch;
-    text[i] = '\0';
-    fclose(file);
-    return SUCCESS;
-}
-
-size_t countOccurrences(char *text, char searchChar) {
-    size_t count=0;
-    for (size_t i=0; i<BUFFSIZE-1 && text[i]!='\0' && text[i]!=EOF; ++i)
-        if (text[i] == searchChar)
-            ++count;
-    return count;
-
-}
-#ifndef isspace
-#define isspace(c) (((c) >= 9 && (c) <= 13) || (c) == 32)
-#endif
-
-void wrap_str(char *start_linep, int lim) {
-    int islastspaces = 0;
-    char *curr_posp;
-    for (curr_posp = start_linep; *curr_posp != '\0'; ++curr_posp) {
-        if (isspace(*curr_posp))
-            islastspaces = 1;
-        if (curr_posp - start_linep >= lim && islastspaces) {
-            while (!isspace(*curr_posp))
-                --curr_posp;
-            *curr_posp++ = '\n';
-            start_linep = curr_posp;
-            islastspaces = 0;
+    if (!lines->lines)
+        return false;
+    char buffer[MAX_LINE_LENGTH];
+    while (fgets(buffer, sizeof(buffer), file)) {
+        if (lines->numLines >= lines->maxLines) {
+            size_t newMaxLines = lines->maxLines * 2;
+            char **newLines = realloc(lines->lines, newMaxLines * sizeof(char *));
+            if (!newLines) {
+                free(lines->lines);
+                return false;
+            }
+            lines->lines = newLines;
+            lines->maxLines = newMaxLines;
         }
+        buffer[strcspn(buffer, "\n")] = '\0';
+        lines->lines[lines->numLines] = strdup(buffer);
+        if (!lines->lines[lines->numLines]) {
+            free(lines->lines);
+            return false;
+        }
+        ++lines->numLines;
     }
+    return true;
 }
 
-#define FILENAME "input.txt"
-#define SEARCH_LETTER 't'
+size_t countOccurrences(const TextLines *lines, char searchChar) {
+    size_t count = 0;
+    for (size_t i = 0; i < lines->numLines; ++i)
+        for (size_t j = 0; j < strlen(lines->lines[i]); ++j)
+            if (lines->lines[i][j] == searchChar)
+                ++count;
+    return count;
+}
+
+void freeTextLines(TextLines *lines) {
+    for (size_t i = 0; i < lines->numLines; ++i)
+        free(lines->lines[i]);
+    free(lines->lines);
+    lines->lines = NULL;
+    lines->numLines = lines->maxLines = 0;
+}
+
+void printTextLines(const TextLines *lines) {
+    for (size_t i = 0; i < lines->numLines; ++i)
+        printf("%zu: %s\n", i, lines->lines[i]);
+}
 
 int main(void) {
-    char text[BUFFSIZE];
-    if (readText(text, FILENAME)) {
-        wrap_str(text, 30);
-        printf("Source text from file %s:\n%s\n", FILENAME, text);
-        printf("The character '%c' appears %zu times.\n",
-               SEARCH_LETTER, countOccurrences(text, SEARCH_LETTER));
-    } else {
-        printf("Failed to read text from the file.\n");
+#ifndef FILENAME
+    printf("Enter the name of the file to read: ");
+    char filename[256];
+    if (!fgets(filename, sizeof(filename), stdin)) {
+        fprintf(stderr, "Error reading file name\n");
+        return EXIT_FAILURE;
     }
+    filename[strcspn(filename, "\n")] = '\0';
+    FILE *file = fopen(filename, "r");
+#else
+    const char *filename = FILENAME;
+    FILE *file = fopen(FILENAME, "r");
+#endif
+    if (!file) {
+        fprintf(stderr, "Could not open file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+    TextLines lines = { NULL };
+    if (readTextFromFile(&lines, file)) {
+        printTextLines(&lines);
+        printf("Enter a character to count occurrences of: ");
+        char searchChar;
+        if (scanf(" %c", &searchChar) != 1) {
+            fprintf(stderr, "Error reading character\n");
+            return EXIT_FAILURE;
+        }
+        printf("the character '%c' occurs %zu times\n", searchChar,
+               countOccurrences(&lines, searchChar));
+    } else {
+        fprintf(stderr, "Error reading from file\n");
+    }
+    fclose(file);
+    freeTextLines(&lines);
     return EXIT_SUCCESS;
 }
